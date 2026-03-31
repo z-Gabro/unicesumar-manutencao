1bee4efc9aaac245c4717d8a16992d3140795525
Gabriel <gabrieldsvr@gmail.com>
2026-03-31 16:47:37 -0300

    fix: consertado erro de crash em listBooksSimple com lista vazia

--- library-maintenance-lab/src/BookManager.java
+++ library-maintenance-lab/src/BookManager.java
@@ -26,11 +26,9 @@ public class BookManager {
             temp.add(e.getValue());
         }
 
-        // TODO: This logic was duplicated from another module.
-        // Can it be centralized?
-        // BUG (edge case): if there are no books this line crashes.
-        if (temp.size() == 0) {
-            System.out.println(temp.get(0));
+        if (temp.isEmpty()) {
+            System.out.println("Nenhum livro encontrado.");
+            return;
         }
 
         System.out.println("ID | TITLE | AUTHOR | Y | CAT | AV");
